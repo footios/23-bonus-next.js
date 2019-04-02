493bce2ab9cbc6890243fdf2bec964f47f74b252
Fotis Tsakiris <footios76@gmail.com>
2019-04-02 13:36:05 +0300

    Git tracking to many files fixed

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import User from '../../components/User'
 
 const authPage = () => {
     return ( 
         <div>
             <h1>The auth page</h1>
+            <User name='fotis' age={42} country='Greecr' />
         </div>
      );
 }
