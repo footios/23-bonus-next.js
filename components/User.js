99323b90aed64bccf7af997c90530938858ecf2c
Fotis Tsakiris <footios76@gmail.com>
2019-04-02 15:37:53 +0300

    418. style added

--- components/User.js
+++ components/User.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const user = (props) => {
-    return ( 
-        <div>
-            <h1>{props.name}</h1>
-            <p>Age: {props.age} </p>
-            <p>Country: {props.country} </p>
-            <p>Occupation: {props.occupation} </p>
-        </div>
-     );
-}
- 
+	return (
+		<div>
+			<h1>{props.name}</h1>
+			<p>Age: {props.age} </p>
+			<p>Country: {props.country} </p>
+			<p>Occupation: {props.occupation} </p>
+			<style jsx>
+				{`
+					div {
+                        width 50%;
+                        margin: auto;
+                        display: inlign-block;
+                        border: 1px solid #eee;
+                        box-shadow: 0 2px 3px #ccc;
+                        padding: 20px;
+                        text-align: center;
+					}
+				`}
+			</style>
+		</div>
+	);
+};
+
 export default user;
